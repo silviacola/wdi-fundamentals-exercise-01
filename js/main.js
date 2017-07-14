ec98a172429efe6e394b93c5a62a70330b10d99f
Silvia Colarossi <silvia.colarossi@gmail.com>
2017-07-14 12:10:55 -0400

    add some logic to the game

--- js/main.js
+++ js/main.js
@@ -1,8 +1,60 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+// console.log('User flipped ' + cardOne);
+// console.log('User flipped ' + cardFour);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+};
+
+var flipCard = function(cardId) {
+  if (cardsInPlay.length === 2) {
+      console.log(console.length)
+  } if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+  } else {
+      alert("Sorry, try again!");
+  }
+  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+};
+ flipCard(0);
+ flipCard(2);
+
+
+//Added in unit 8
+    //   var cardOne = cards[0];
+    //     cardsInPlay.push(cardOne);
+    //     console.log('User flipped ' + cardOne);
+
+    //   var cardTwo = cards[2];
+    //     cardsInPlay.push(cardTwo);
+    //     console.log('User flipped ' + cardTwo);
+
+
+    // if (cardsInPlay.length === 2) {
+    //     console.log(console.length)
+    // } if (cardsInPlay[0] === cardsInPlay[1]) {
+    //     alert("You found a match!");
+    //  } else {
+    //     alert("Sorry, try again!");
+    //  }
+
+
+
+//Write an if statement that checks to see if the length of the cardsInPlay array is 2.
+//For the condition, you can use the length property to find out how many items are in the cardsInPlay array and then use the === operator to see if the length is equal to 2.
+//Now we'll want to add logic that checks for equality between the two cards in the cardsInPlay array.
+
+//Inside of the if statement you created in the last step, create an if...else statement that checks for equality between the two cards in the cardsInPlay array.
+//For the condition, check to see if the first card in the cardsInPlay array (cardsInPlay[0]) is equal to the second card in the cardsInPlay array.
+
+//Hint: You'll want to use the === operator here.
