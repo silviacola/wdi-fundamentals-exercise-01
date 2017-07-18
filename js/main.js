b8a54d7c106af292fe3a8aca37f9e84a239dacf9
Silvia Colarossi <silvia.colarossi@gmail.com>
2017-07-18 00:16:55 -0400

    went through half of last step unit 11, but still can't figure out what's wrong

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+  // Check for equality between the 2 cards in the cardsInPlay array
   if (cardsInPlay[0] === cardsInPlay[2]) {
     alert("You found a match!");
     } else {
@@ -43,31 +44,27 @@ var checkForMatch = function() {
 
 // We won't want a check for a match until the user
 // has selected 2 cards.
-// cardsId is the index number that is passed in when
+// cardId is the index number that is passed in when
 // the flipCard function is called to access this card
 // from the cards array.
-var flipCard = function() {
+var flipCard = function () {
   // declaring cardId - not sure
-  document.getElementById('cardsId');
+  var cardId = this.getAttribute('data-id');
 
   // Displaying the card that was flipped over:
   // Updated with .rank to access the rank property of this object
-  console.log("User flipped " + cards[cardsId].rank);
-
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   // adding that card to the cardsInPlay array
-  cardsInPlay.push(cards[cardsId].rank);
-
+  cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardID].cardImage);
   if (cardsInPlay.length === 2) {
-  console.log(cardsInPlay.length);
-  // Check for equality between the 2 cards in the cardsInPlay array
-  }
-
   //calling the checkForMatch function
   checkForMatch();
+  }
+};
 
-  console.log(cards.cardImage[cardsId]);
-  console.log(cards.suit[cardsId]);
-}
 // simulating the user flipping two cards
 // deleted in unit 11
 // flipCard(cards[0]);
@@ -79,7 +76,7 @@ var createBoard = function () {
   var cardElement = document.createElement('img');
   // use the setAtrribute() method to add a src attribute
   // for the cardElement
-  cardElement.setAttribute('cardElement', src = "images/back.png");
+  cardElement.setAttribute('src', "images/back.png");
   // set the card's 'data-id' attribute to be
   //the index of the current element, i (no quotes).
   cardElement.setAttribute('data-id', i);
@@ -89,10 +86,10 @@ var createBoard = function () {
   cardElement.addEventListener('click', 'flipCard');
   // use appendChild() method to append the current cardElement
   // to the game board which as an id of game-board'
-  cardElement.appendChild('game-board');
+  document.getElementByID('game-board').appendChild(cardElement);
  }
 }
-gameboard();
+createBoard();
 
 
 // INSTRUCTIONS
